be320e133f6d518b28e8d14644bd37c9ac4b9a34
Yunjeon-Lee <yjeonlee0905@gmail.com>
2021-11-11 17:06:01 -0500

    [lab3] WIP: parsing probability

--- lab3/Parser.py
+++ lab3/Parser.py
@@ -1,11 +1,14 @@
 class Parser:
 
-    def parse(self, fileName):
-        chanceNodes = {}
-        decisionNodes = {}
-        rewards = {}
-        edges = {}
+    def __init__(self):
+        self.chanceNodes = {}
+        self.decisionNodes = {}
+        self.rewards = {}
+        self.edges = {}
+        self.nodes = set()
+        self.terminals = []
 
+    def parse(self, fileName):
         lines = open(fileName, 'r').read().split('\n')
 
         for line in lines:
@@ -23,24 +26,52 @@ class Parser:
 
                 # split probabilities
                 if len(splitedValues) == 1:
-                    decisionNodes[key] = splitedValues[0]
+                    self.decisionNodes[key] = splitedValues[0]
                 else:
-                    chanceNodes[key] = splitedValues
+                    self.chanceNodes[key] = splitedValues
             elif '=' in line: # rewards
                 splited = line.split('=')
                 key = splited[0].strip()
                 value = splited[1].strip()
-                rewards[key] = value
+                self.rewards[key] = value
             elif ':' in line:
                 splited = line.split(':')
                 key = splited[0].strip()
                 value = splited[1].strip()
+                self.nodes.add(key)
 
                 value = value.replace('[', '')
                 value = value.replace(']', '')
                 splitedValues = value.split(',')
                 for i in range(0, len(splitedValues)):
                     splitedValues[i] = splitedValues[i].strip()
-                edges[key] = splitedValues
+                    self.nodes.add(splitedValues[i])
+                self.edges[key] = splitedValues
 
         print(lines)
+
+    def setRewards(self):
+        for key in self.edges.keys():
+            if key not in self.rewards:
+                self.rewards[key] = 0
+
+    def setAndValidateTerminals(self):
+        for node in self.nodes:
+            if node not in self.edges:
+                self.terminals.append(node)
+
+    def parseAndValidateProbabilites(self):
+        print(self.decisionNodes)
+        print(self.chanceNodes)
+
+        for chanceNode in self.chanceNodes:
+            if chanceNode.count('.') == 1:
+                idx = chanceNode.find('.')
+                numerator = chanceNode[idx+1:].strip()
+                denominator = pow(10, len(numerator))
+                decimal = int(numerator) / denominator
+
+
+            else:
+                print chanceNode + ' has wrong format of probability'
+                # TODO exit?
--- lab3/main.py
+++ lab3/main.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
 
     parser = Parser()
     parser.parse(fileName)
+    parser.parseAndValidateProbabilites()
