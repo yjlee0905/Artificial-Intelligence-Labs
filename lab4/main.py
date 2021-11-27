0b83191c4a1c67c4fcef244869c5f76a7cbb911c
Yunjeon-Lee <yjeonlee0905@gmail.com>
2021-11-27 03:03:19 -0500

    [lab4] knn - add output

--- lab4/main.py
+++ lab4/main.py
@@ -3,14 +3,15 @@ from KMeansAlgorithm import KMeansAlgorithm
 
 if __name__ == "__main__":
 
-    fileName = "/Users/yjeonlee/Desktop/[Fall2021]AI/AI-Python/lab4/inputs/km1.txt"
-    kmeans = KMeansAlgorithm()
-    kmeans.parseFile(fileName)
-    kmeans.kmeans([[0,0], [200,200], [500,500]])
+    # fileName = "/Users/yjeonlee/Desktop/[Fall2021]AI/AI-Python/lab4/inputs/km1.txt"
+    # kmeans = KMeansAlgorithm()
+    # kmeans.parseFile(fileName)
+    # kmeans.kmeans([[0,0], [200,200], [500,500]])
 
-    # fileName = "/Users/yjeonlee/Desktop/[Fall2021]AI/AI-Python/lab4/inputs/knn1.train.txt"
-    # knn = KNNalgorithm()
-    # knn.parseFile(fileName)
-    # knn.decideCluster(3)
+    train = "/Users/yjeonlee/Desktop/[Fall2021]AI/AI-Python/lab4/inputs/knn3.train.txt"
+    test = "/Users/yjeonlee/Desktop/[Fall2021]AI/AI-Python/lab4/inputs/knn3.test.txt"
+    knn = KNNalgorithm()
+    knn.parseFiles(train, test)
+    knn.knn(3)
 
 
