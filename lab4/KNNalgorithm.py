ffcd0459076b8e2cb8f42b3ec04cee7a31200398
Yunjeon-Lee <yjeonlee0905@gmail.com>
2021-12-05 01:08:41 -0500

    [lab4] add Precision and Recall

--- lab4/KNNalgorithm.py
+++ lab4/KNNalgorithm.py
@@ -3,10 +3,16 @@ from DistanceCalculator import DistanceCalculator
 class KNNalgorithm:
 
     def __init__(self):
-        self.train = []
-        self.test = []
         self.calculator = DistanceCalculator()
         # {[p1, p2]: label}
+        self.train = []
+        self.test = []
+        self.labels = set()
+
+        # Precision and Recall
+        self.numGot = {}
+        self.numWant = {}
+        self.numEquals = {}
 
 
     def parseFiles(self, trainFile, testFile):
@@ -20,9 +26,11 @@ class KNNalgorithm:
         data = []
         for line in lines:
             splitted = line.split(',')
+            label = splitted[-1]
             strPoints = splitted[:-1]
             point = [int(num) for num in strPoints]
             point.append(splitted[-1])
+            self.labels.add(label)
             data.append(point)
         return data
 
@@ -45,6 +53,36 @@ class KNNalgorithm:
             neighbors = sortedDist[:k]
             newCluster = self.selectClusterByWeighted(neighbors)
             print "want=" + self.test[test][-1] + " got=" + newCluster
+
+            # Evaluation
+            if newCluster in self.numGot:
+                self.numGot[newCluster] += 1
+            else:
+                self.numGot[newCluster] = 1
+
+            if self.test[test][-1] in self.numWant:
+                self.numWant[self.test[test][-1]] += 1
+            else:
+                self.numWant[self.test[test][-1]] = 1
+
+            if self.test[test][-1] == newCluster:
+                if newCluster in self.numEquals:
+                    self.numEquals[newCluster] += 1
+                else:
+                    self.numEquals[newCluster] = 1
+
+        keys = list(self.labels)
+        keys.sort()
+        for key in keys:
+            if key not in self.numWant:
+                self.numWant[key] = 0
+            if key not in self.numGot:
+                self.numGot[key] = 0
+            if key not in self.numEquals:
+                self.numEquals[key] = 0
+
+        for key in keys:
+            print "Label=" + key + " Precision=" + str(self.numEquals[key]) + "/" + str(self.numGot[key]) + " Recall=" + str(self.numEquals[key]) + "/" + str(self.numWant[key])
 
 
     def selectCluster(self, neighbors):
@@ -77,4 +115,3 @@ class KNNalgorithm:
                 vote[neighbor[-1]] = dist
         selected = max(vote, key=vote.get)
         return selected
-
